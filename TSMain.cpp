385932c1d2a28976e47b83e014ff544bfa307cf9
Steven Tucker <srt1500@rit.edu>
2013-04-29 11:41:56 -0400

    Removed inbox nips

--- TSMain.cpp
+++ TSMain.cpp
@@ -155,19 +155,9 @@ DWORD WINAPI handleMail(LPVOID lpParam)
                         fout.close();
                     }
                 }
-
-            }
-                if (recMessage.substr(0,5) != "INBOX")
-                {
-                    cout << "INBOX wasn't opened.. Wrong Command.\n";
-                    current_client.sendData(Status::SMTP_CMD_SNTX_ERR); //send error
-                }
-                else
-                {
-                    
-                }
             }
         }
+    }
 
         //get data from the client before starting loop again
         clientFlop = current_client.recvData(recMessage);
